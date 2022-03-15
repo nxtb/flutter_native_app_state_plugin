d66813a7e4a06927833cd928ad5a1276cda50e3b
Jon Phipps <59151421+phipps980316@users.noreply.github.com>
2022-03-15 16:00:17 +0000

    migrate to null safe

--- lib/flutter_native_app_state.dart
+++ lib/flutter_native_app_state.dart
@@ -6,7 +6,7 @@ class FlutterNativeAppState {
   static const EventChannel _eventChannel =
       const EventChannel('flutter_native_app_state/events');
 
-  static Stream<int> _appStateStream;
+  static Stream<int>? _appStateStream;
   static Stream<int> get observeAppState {
     return _appStateStream ??=
         _eventChannel.receiveBroadcastStream().map<int>((event) => event);
